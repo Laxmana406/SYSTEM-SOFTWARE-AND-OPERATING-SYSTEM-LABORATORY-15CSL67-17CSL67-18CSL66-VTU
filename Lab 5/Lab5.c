dc8d60d43dc0ce157fef0e0daac57a3863ce205d
Ponnanna M.B <50457876+PonnannaMB@users.noreply.github.com>
2019-05-11 09:01:31 +0530

    Update Lab5.c

--- Lab 5/Lab5.c
+++ Lab 5/Lab5.c
@@ -1,3 +1,5 @@
+/*5. Design, develop and implement a C/Java program to generate the machine code using Triples for the statement A = -B * (C +D) 
+whose intermediate code in three-address form:  T1 = -B   T2 = C + D    T3 = T1 + T2     A = T3 */
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
